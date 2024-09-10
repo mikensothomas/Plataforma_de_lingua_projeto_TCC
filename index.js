9713218601edc2ca77825b121be2ca6ad4e97251
mikensothomas <thomasmikenson20@gmail.com>
2024-09-10 13:41:36 -0300

    Commit

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+// // Configura o diretório de visualizações e o mecanismo de visualização
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+// // Corrija o caminho para o roteador
+// const mainRouter = require('./routes/index'); // Caminho correto para o arquivo de roteador
+// app.use('/', mainRouter);
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Servidor rodando na porta ${port}`);
+// });
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -6,7 +23,11 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Corrija o caminho para o roteador
+// Configura o diretório para arquivos estáticos (CSS, JS, etc.)
+app.use('/stylesheets', express.static(path.join(__dirname, 'public/stylesheets')));
+app.use('/javascripts', express.static(path.join(__dirname, 'public/javascripts')));
+
+// Corrige o caminho para o roteador
 const mainRouter = require('./routes/index'); // Caminho correto para o arquivo de roteador
 app.use('/', mainRouter);
 
