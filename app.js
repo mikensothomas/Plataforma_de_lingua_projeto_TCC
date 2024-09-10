98486d9b1cfbbee450eb287c8a19e956c73b5c64
mikensothomas <thomasmikenson20@gmail.com>
2024-09-10 14:36:55 -0300

    Commit

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv');
+const connectDb = require('./bd.js');
+const session = require('express-session');
+const bcryptjs = require('bcryptjs');
+const { Client } = require('pg');
+const jwt = require('jsonwebtoken');
+// var port = normalizePort(process.env.PORT || '3000');
+// app.set('port', port);
+
+const flash = require('connect-flash');
+
+const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
+
+dotenv.config();
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
+
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 3 * 60 * 60 * 1000 }
+}));
 
 // Configura o diretório de visualizações e o mecanismo de visualização
 app.set('views', path.join(__dirname, 'views'));
@@ -19,3 +55,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
+
+module.exports = app;
