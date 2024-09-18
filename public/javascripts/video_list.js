a2195d162ff7a59bb9fc8a571cb1ad26e3a56ff5
mikensothomas <thomasmikenson20@gmail.com>
2024-09-17 23:29:19 -0300

    Commit

--- public/javascripts/video_list.js
+++ public/javascripts/video_list.js
@@ -1,16 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const mensagemSucessosLogin = localStorage.getItem('cadastroMensagem');
-    if (mensagemSucessosLogin) {
-        const mensagemSucessoDiv = document.querySelector('.mensagemSucessosLogin');
-        mensagemSucessoDiv.textContent = mensagemSucessosLogin;
-        mensagemSucessoDiv.style.opacity = '1';
-        setTimeout(() => {
-            mensagemSucessoDiv.style.opacity = '0';
-        }, 5000);
-
-        localStorage.removeItem('cadastroMensagem');
-    }
-});
 
 document.addEventListener('DOMContentLoaded', function () {
     const langToggle = document.querySelector('.lang-toggle');
